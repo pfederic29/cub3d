51b96e9101c4c4c8c9f8e021ea629b8ab5b3f076
Luca Marzano <lmarzano@student.42roma.it>
2021-03-17 12:04:32 +0100

    rgb parsing funzionante. struttura popolata fino alla mappa

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lmarzano <lmarzano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 16:36:02 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/03/16 17:43:11 by lmarzano         ###   ########.fr       */
+/*   Updated: 2021/03/17 12:02:43 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,13 +51,20 @@ int	main()
 	while (i)
 	{
 		i = get_next_line(fd_map, line);
+		
 		if(i == 1)
     	    i = parsing(line);
 		else
 			write(1, "Error!\n", 7);
 	}
-	printf("\n|heigth: %d|\n", g_p.res_h);
 	printf("\n|width: %d|\n", g_p.res_w);
-	printf("\n|NO: %d, %d, %d|\n", g_p.n_wall[0], g_p.n_wall[1], g_p.n_wall[2]);
+	printf("|heigth: %d|\n", g_p.res_h);
+	printf("|NO: %d, %d, %d|\n", g_p.n_wall[0], g_p.n_wall[1], g_p.n_wall[2]);
+	printf("|SO: %d, %d, %d|\n", g_p.s_wall[0], g_p.s_wall[1], g_p.s_wall[2]);
+	printf("|WE: %d, %d, %d|\n", g_p.w_wall[0], g_p.w_wall[1], g_p.w_wall[2]);
+	printf("|EA: %d, %d, %d|\n", g_p.e_wall[0], g_p.e_wall[1], g_p.e_wall[2]);
+	printf("|S: %d, %d, %d|\n", g_p.sky[0], g_p.sky[1], g_p.sky[2]);
+	printf("|F: %d, %d, %d|\n", g_p.floor[0], g_p.floor[1], g_p.floor[2]);
+	printf("|C: %d, %d, %d|\n", g_p.sky[0], g_p.ceiling[1], g_p.ceiling[2]);
 	return (0);
 }
--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: lmarzano <lmarzano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 14:36:05 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/03/16 19:36:48 by lmarzano         ###   ########.fr       */
+/*   Updated: 2021/03/17 12:04:12 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,67 @@
 
 int parsing(char **line)
 {
-	if (*line[0] == 'R')
+	if ((*line)[0] == 'R')
 		res_parse(line);
-	else if (*line[0] == 'N')
+	else if ((*line)[0] == 'N')
 	{
 		if ((*line)[1] != 'O')
 			return (-1);
 		rgb_parse(line, g_p.n_wall);
+		if (g_p.n_wall[0] > 256 || g_p.n_wall[1] > 256 || g_p.n_wall[2] > 256)
+			return (-1);
 	}
-	//else if (*line[0] != '\n')
-	//	return (-1);
+	else if ((*line)[0] == 'S' && (*line)[1] == 'O')
+	{
+		rgb_parse(line, g_p.s_wall);
+		if (g_p.s_wall[0] > 256 || g_p.s_wall[1] > 256 || g_p.s_wall[2] > 256)
+			return (-1);
+	}
+	else if ((*line)[0] == 'S' && (*line)[1] == ' ')
+	{
+		rgb_parse(line, g_p.sky);
+		if (g_p.sky[0] > 256 || g_p.sky[1] > 256 || g_p.sky[2] > 256)
+			return (-1);
+	}
+	else if ((*line)[0] == 'W')
+	{
+		if ((*line)[1] != 'E')
+			return (-1);
+		rgb_parse(line, g_p.w_wall);
+		if (g_p.w_wall[0] > 256 || g_p.w_wall[1] > 256 || g_p.w_wall[2] > 256)
+			return (-1);
+	}
+	else if ((*line)[0] == 'E')
+	{
+		if ((*line)[1] != 'A')
+			return (-1);
+		rgb_parse(line, g_p.e_wall);
+		if (g_p.e_wall[0] > 256 || g_p.e_wall[1] > 256 || g_p.e_wall[2] > 256)
+			return (-1);
+	}
+	else if ((*line)[0] == 'F')
+	{
+		if ((*line)[1] != ' ')
+			return (-1);
+		rgb_parse(line, g_p.floor);
+		if (g_p.floor[0] > 256 || g_p.floor[1] > 256 || g_p.floor[2] > 256)
+			return (-1);
+	}
+	else if ((*line)[0] == 'C')
+	{
+		if ((*line)[1] != ' ')
+			return (-1);
+		rgb_parse(line, g_p.ceiling);
+		if (g_p.ceiling[0] > 256 || g_p.ceiling[1] > 256 || g_p.ceiling[2] > 256)
+			return (-1);
+	}
+	else if ((*line)[1] == ' ' || (*line)[1] == '1')
+	{
+		return (1);
+		//funzione di parsing della mappa
+	}
+	else if ((*line)[0] != '\n' || (*line)[0] != 0)
+		return (-1);
 	return (1);
 }
 
