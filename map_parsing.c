59cf9e4ee99aa4b818c853dce119637aa829f4e2
Luca Marzano <lmarzano@student.42roma.it>
2021-03-30 15:34:33 +0200

    202 vs. 115

--- map_parsing.c
+++ map_parsing.c
@@ -6,7 +6,7 @@
 /*   By: lmarzano <lmarzano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 14:28:34 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/03/30 13:34:35 by lmarzano         ###   ########.fr       */
+/*   Updated: 2021/03/30 15:29:17 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,17 +93,26 @@ int	sfc_tx(char **line, int j)
 	if ((*line)[j] == 'S' && g_check.s == 0)
 	{
 		g_p.sfc[0] = sfc_store(line);
-		g_check.s = 1;
+		if (!(g_p.sfc[0]))
+			return (-1);
+		else
+			g_check.s = 1;
 	}
 	else if ((*line)[j] == 'F'&& g_check.f == 0)
 	{
 		g_p.sfc[1] = sfc_store(line);
-		g_check.f = 1;
+		if (!(g_p.sfc[1]))
+			return (-1);
+		else
+			g_check.f = 1;
 	}
 	else if ((*line)[j] == 'C' && g_check.c == 0)
 	{
 		g_p.sfc[2] = sfc_store(line);
-		g_check.c = 1;
+		if (!(g_p.sfc[2]))
+			return (-1);
+		else
+			g_check.c = 1;
 	}
 	else
 		g_check.err = -1;
